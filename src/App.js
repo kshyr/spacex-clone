de10b4607039444efff1df97bccaad22dbe73dad
DEKO95 <shirolapov.kostya@gmail.com>
2021-04-15 02:51:38 -0400

    redirect, router, background image

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
 import React from 'react'
 import Header from './components/Header'
+import {Link, BrowserRouter as Router, HashRouter} from 'react-router-dom'
+import {Route, Redirect} from 'react-router-dom'
+import HomePage from './components/HomePage'
 
 
 class App  extends React.Component {
   render() {
     return(
+
       <div className="app">
-      <Header />
+        <div className="header"><Header style={{backgroundImage: 'url(https://www.spacex.com/static/images/backgrounds-2021/HP_SN11_Vertical_20210311_IMG_01221.webp)'}}/></div>
+        <div className="homepage"></div>
+        
+
+
+
+
+
+        <Router>
+          <Route exact path="/vehicles">
+            <Redirect to="/" />
+          </Route>
+        </Router>
+        <Router>
+          
+        </Router>
       </div>
 
     )
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,27 @@
 import React, {Component} from 'react'
 import './Header.css'
-import {Link} from 'react'
+import {Link, BrowserRouter as Router, } from 'react-router-dom'
 
 class Header extends Component {
     render() {
-        return(<div>
-            <div className="logo">
-                <img className="spacex__logo" src="https://logos-world.net/wp-content/uploads/2020/09/SpaceX-Logo.png" alt="" />
-            </div>
-            <nav>
-                <ul>
-                    <li></li>
-                </ul>
-            </nav>
-           
-            </div>
+        return(<>
+                <Router>
+                    <div className="header">
+                        <div className="backgroundimage">
+                            <Link to="/"><img className="spacex__logo" src="https://logos-world.net/wp-content/uploads/2020/09/SpaceX-Logo.png" alt="" /></Link>
+                            <ul className="Routes">
+                                <li><Link to="/vehiles/falcon-9">FALCON 9</Link></li>
+                                <li><Link to="/vehiles/falcon-heavy">FALCON HEAVY</Link></li>
+                                <li><Link to="/vehiles/dragon">DRAGON</Link></li>
+                                <li><Link to="/vehiles/starship">STARSHIP</Link></li>
+                                <li><Link to="/vehiles/human-spaceflight">HUMAN SPACEFLIGHT</Link></li>
+                                <li><Link to="/vehiles/rideshare">RIDESHARE</Link></li>
+                                <li><Link to="shop">Shop</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </Router>
+            </>
         )
     }
 }
