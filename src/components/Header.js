f7d024a31b8a3d8f59c9d119106aa3c3bf5dbbd5
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-04-07 13:52:52 -0400

    added animations to header links, figuring out the best approach to reloading page with react-router.

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,18 +7,18 @@ const Header = () => {
     <Router>
       <div className="header">
         <div className="logo">
-          <Link to="/">
+          <Link to="/" onClick={() => {window.location.href="/"}}>
             <img className="spacex__logo" src={process.env.PUBLIC_URL + "/spacex-logo.svg"} alt="" />
           </Link>
         </div>
-        <ul className="Routes">
-          <li><Link to="/vehicles/falcon-9">FALCON 9</Link></li>
-          <li><Link to="/vehicles/falcon-heavy">FALCON HEAVY</Link></li>
-          <li><Link to="/vehicles/dragon">DRAGON</Link></li>
-          <li><Link to="/vehicles/starship">STARSHIP</Link></li>
-          <li><Link to="/vehicles/human-spaceflight">HUMAN SPACEFLIGHT</Link></li>
-          <li><Link to="/vehicles/rideshare">RIDESHARE</Link></li>
-          <li><Link to="shop">SHOP</Link></li>
+        <ul className="routes">
+          <li><Link to="/falcon-9" onClick={() => {window.location.href="/falcon-9"}}>FALCON 9</Link></li>
+          <li><Link to="/falcon-heavy">FALCON HEAVY</Link></li>
+          <li><Link to="/dragon">DRAGON</Link></li>
+          <li><Link to="/starship">STARSHIP</Link></li>
+          <li><Link to="/human-spaceflight">HUMAN SPACEFLIGHT</Link></li>
+          <li><Link to="/rideshare">RIDESHARE</Link></li>
+          <li><Link to="/shop">SHOP</Link></li>
         </ul>
       </div>
     </Router>
