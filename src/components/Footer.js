94c59dbcd8e60ad910b87572626bdb3dbe63393b
DEKO95 <60661103+DEKO95@users.noreply.github.com>
2022-04-07 13:47:52 -0400

    added footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,16 @@
 import React from 'react';
-
+import './Footer.css';
 const Footer = () => {
   return (
-    <div>
-      
+    <div className="footer">
+      <ul className="links">
+        <li><a href="#">TWITTER</a></li>
+        <li><a href="#">YOUTUBE</a></li>
+        <li><a href="#">INSTAGRAM</a></li>
+        <li><a href="#">FLICKR</a></li>
+        <li><a href="#">LINKEDIN</a></li>
+        <li><a href="#">PRIVACY POLICY</a></li>
+      </ul>
     </div>
   )
 }
